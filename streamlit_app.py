00655f0f7e451afefadc8a70085d3dd1d2a62af5
Prasad Pawar <167980475+Unknownuser0419@users.noreply.github.com>
2024-04-24 17:35:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.title("Hello world!")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   data = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(data)
 
 
 
